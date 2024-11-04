f74e1fc7339e8f2b6c4348a900b4c73428b33e6c
jjxmonster <kuba.tarabasz@wp.pl>
2024-11-04 23:08:16 +0100

    S01E01 Firmware mission

---
+++ lessons/S01E01/index.ts
@@ -0,0 +1,60 @@
+import puppeteer from "puppeteer";
+import OpenAI from "openai";
+
+async function automateAnswer(): Promise<string> {
+	const browser = await puppeteer.launch({ headless: true });
+	const page = await browser.newPage();
+	await page.goto("https://xyz.ag3nts.org/");
+	console.log("Navigated to https://xyz.ag3nts.org/");
+	await page.type('input[name="username"]', "tester");
+	console.log("Typed username");
+	await page.type('input[name="password"]', "574e112a");
+	console.log("Typed password");
+
+	const questionElement = await page.$("#human-question");
+	const question = await page.evaluate(
+		el => el.textContent?.trim(),
+		questionElement
+	);
+
+	const answer = await solveQuestionWithOpenAI(question);
+
+	await page.type('input[name="answer"]', answer);
+	console.log("Typed answer");
+
+	// Wprowadzanie odpowiedzi
+	await page.click('button[type="submit"]');
+	console.log("Clicked submit");
+	await page.waitForNavigation();
+	console.log("Navigated to next page");
+
+	const content = await page.content();
+	console.log("Page:", content);
+
+	await browser.close();
+
+	return content;
+}
+
+async function solveQuestionWithOpenAI(question: string): Promise<string> {
+	const openai = new OpenAI();
+
+	const prompt = `
+    You are a helpful assistant that can answer questions about dates.
+    Answer the following question with only the year and noting else: ${question}
+    `;
+
+	try {
+		const completion = await openai.chat.completions.create({
+			model: "gpt-4o-mini",
+			messages: [{ role: "user", content: prompt }],
+		});
+
+		const answer = completion.choices[0].message.content;
+		return answer || "000";
+	} catch (error) {
+		return "0000";
+	}
+}
+
+automateAnswer();
