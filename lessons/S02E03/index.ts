071cbd84f2ef5a39d61930412875edea7a1bcbe3
jjxmonster <kuba.tarabasz@wp.pl>
2024-11-19 21:38:47 +0100

    S02E03 Robot image generation

---
+++ lessons/S02E03/index.ts
@@ -0,0 +1,29 @@
+import OpenAI from "openai";
+
+const openai = new OpenAI();
+
+export const getRobotDescription = async () => {
+	const response = await fetch(
+		`https://centrala.ag3nts.org/data/${process.env.AI_DEVS_KEY}/robotid.json`
+	);
+	const data = (await response.json()) as { description: string };
+
+	return data.description;
+};
+
+const generateRobotImage = async () => {
+	const robotDescription = await getRobotDescription();
+
+	const response = await openai.images.generate({
+		model: "dall-e-3",
+		prompt: robotDescription,
+	});
+
+	const imageUrl = response.data[0].url;
+
+	return imageUrl;
+};
+
+const image = await generateRobotImage();
+
+console.log(image);
